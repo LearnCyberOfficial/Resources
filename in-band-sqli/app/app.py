2a0e48e4485e70062d9914b875233a09040a9e44
Et3rnos <57318423+Et3rnos@users.noreply.github.com>
2023-07-30 20:43:35 +0100

    in-band-sqli

---
+++ in-band-sqli/app/app.py
@@ -0,0 +1,29 @@
+from flask import *
+import mysql.connector
+import os
+
+app = Flask(__name__)
+db = mysql.connector.connect(
+  host="database",
+  user="root",
+  db="db",
+  password="password"
+)
+
+cursor = db.cursor()
+
+try:
+    cursor.execute("CREATE TABLE users (username VARCHAR(255), password VARCHAR(255)); INSERT INTO users (username, password) VALUES ('user', 'normalpassword'), ('admin', 'supersecurepassword');")
+except:
+    pass
+
+@app.route("/", methods = ["POST", "GET"])
+def index():
+    if request.method == "POST":
+        password = request.form.get("password")
+        cursor.execute(f"SELECT * FROM users WHERE password = '{password}'")
+        user = cursor.fetchone()
+        return render_template("index.html", user = user, invalid = user is None)
+    return render_template("index.html")
+
+app.run("0.0.0.0", 8080, debug = True)
